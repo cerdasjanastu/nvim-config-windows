b276d7602fdaa7741f1dda6207021fd4244e9202
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-19 21:29:24 +0800

    small update

--- lua/cerdas/plugins/treesitter.lua
+++ lua/cerdas/plugins/treesitter.lua
@@ -2,71 +2,28 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        -- version = false,
-        -- lazy = vim.fn.argc(-1) == 0,
-        -- -- init = function(plugin)
-        -- --     -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-        -- --     -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-        -- --     -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-        -- --     -- Luckily, the only things that those plugins need are the custom queries, which we make available
-        -- --     -- during startup.
-        -- --     require("lazy.core.loader").add_to_rtp(plugin)
-        -- --     require("nvim-treesitter.query_predicates")
-        -- -- end,
-        --
-        -- cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-        -- keys = {
-        --     { "<c-space>", desc = "Increment Selection" },
-        --     { "<bs>", desc = "Decrement Selection", mode = "x" },
-        -- },
-        -- opts_extend = { "ensure_installed" },
-        -- opts = {
-        --     highlight = { enable = true },
-        --     indent = { enable = true },
-        --     ensure_installed = {
-        --         "bash",
-        --         "diff",
-        --         "html",
-        --         "javascript",
-        --         "jsdoc",
-        --         "json",
-        --         "jsonc",
-        --         "lua",
-        --         "luadoc",
-        --         "luap",
-        --         "markdown",
-        --         "markdown_inline",
-        --         "printf",
-        --         "python",
-        --         "query",
-        --         "regex",
-        --         "toml",
-        --         "tsx",
-        --         "typescript",
-        --         "vim",
-        --         "vimdoc",
-        --         "xml",
-        --         "yaml",
-        --     },
-        --     incremental_selection = {
-        --         enable = true,
-        --         keymaps = {
-        --             init_selection = "<C-space>",
-        --             node_incremental = "<C-space>",
-        --             scope_incremental = false,
-        --             node_decremental = "<bs>",
-        --         },
-        --     },
-        --     textobjects = {
-        --         move = {
-        --             enable = true,
-        --             goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-        --             goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-        --             goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-        --             goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-        --         },
-        --     },
-        -- },
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = {
+                    "lua",
+                    "python",
+                    "json",
+                    "xml",
+                    "html",
+                    "css",
+                    "javascript",
+                },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                }
+            })
+        end
     },
 
     {
