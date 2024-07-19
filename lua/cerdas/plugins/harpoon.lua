a5fecd1c442db4d6675908d250b0f8f1e9170031
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-19 22:23:04 +0800

    remove keymap C-c

--- lua/cerdas/plugins/harpoon.lua
+++ lua/cerdas/plugins/harpoon.lua
@@ -44,7 +44,6 @@ return {
             -- Toggle previous & next buffers stored within Harpoon list
             vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
             vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-            vim.keymap.set("n", "<C-c>", function() harpoon:list():next() end)
 
         end
     }
